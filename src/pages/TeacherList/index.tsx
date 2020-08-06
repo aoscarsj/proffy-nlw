f742588d15487fdd4f43817d3117b656cc247dec
Artur <artur.junior@ufba.br>
2020-08-06 19:17:24 -0300

    ..

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        teste
+        <form id="search-teachers">
+          <input></input>
+        </form>
+      </PageHeader>
+    </div>
+  );
+}
+
+export default TeacherList;
